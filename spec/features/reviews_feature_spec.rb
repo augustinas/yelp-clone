7569189eb623d68ce6af7a2d5fb1f5c4345d9abe
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-05-06 17:11:35 +0100

    failing test added for average scores

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 feature 'reviewing' do
 
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
+
   before { Restaurant.create name: 'KFC' }
 
   scenario 'allow users to leave a review using a form' do
@@ -12,6 +21,12 @@ feature 'reviewing' do
     click_button 'Leave Review'
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content 'so so'
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('so so', '3')
+    leave_review('great', '5')
+    expect(page).to have_content 'Average rating: 4'
   end
 
 end
