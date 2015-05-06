5a5eeaaefba25bf228bd1a412c3b544e3d6df2d3
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-06 11:07:16 +0100

    Added restaurant uniqueness and rating validations

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   it { is_expected.to belong_to :restaurant }
+
+  it 'is invalid if the rating is more than 5 or less than 1' do
+    review = Review.new rating: 10
+    expect(review).to have(1).error_on :rating
+
+    review = Review.new rating: -1
+    expect(review).to have(1).error_on :rating
+  end
 end
